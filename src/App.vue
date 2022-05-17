8d7a949c1f4f96c82caa988c5594e389ad0d79b5
Jakub-Izbicki <jakub.izbicki@wundermanthompson.com>
2022-05-18 00:09:57 +0200

    Add default component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <ThreejsScene></ThreejsScene>
 </template>
+
+<script setup lang="ts">
+import ThreejsScene from "./components/ThreejsScene.vue";
+</script>
 
 <style>
 #app {
@@ -15,7 +12,5 @@ import HelloWorld from './components/HelloWorld.vue'
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ThreejsScene.vue
@@ -0,0 +1,6 @@
+<template>
+<p>threejs</p>
+</template>
+
+<script setup lang="ts">
+</script>

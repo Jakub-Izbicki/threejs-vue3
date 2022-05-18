f0a78efd4bab4a8731a488996a680b46e35e4485
Jakub-Izbicki <jakub.izbicki@wundermanthompson.com>
2022-05-18 23:58:43 +0200

    Add onresize, shadows, more cubes

--- src/components/ThreejsScene.vue
+++ src/components/ThreejsScene.vue
@@ -24,39 +24,69 @@ camera.lookAt(0, 0, 0);
 
 const ground = new THREE.Mesh(new THREE.BoxGeometry(10, 1, 10), new THREE.MeshPhongMaterial({
   color: 0x777777,
-  flatShading: true
 }));
 ground.receiveShadow = true;
-const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshPhongMaterial({color: 0x325ea8, flatShading: true}));
-cube.castShadow = true;
+ground.rotation.y = Math.PI / 4;
 
+const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshPhongMaterial({color: 0x325ea8}));
+cube.castShadow = true;
+cube.receiveShadow = true;
+cube.position.x = -2;
 cube.position.y = 1;
-cube.rotation.y = Math.PI * (1/4);
+cube.rotation.y = Math.PI * (1 / 4);
+
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshPhongMaterial({color: 0x325ea8}));
+cube2.castShadow = true;
+cube2.receiveShadow = true;
+cube2.position.x = -1;
+cube2.position.y = 1;
+cube2.position.z = 1.5;
+cube2.rotation.y = Math.PI / 4;
 
 const axesHelper = new THREE.AxesHelper(2);
 cube.add(axesHelper);
 
+const ball = new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshPhongMaterial({color: 0x325ea8}));
+ball.castShadow = true;
+ball.receiveShadow = true;
+ball.position.x = 2;
+ball.position.y = 1;
+
 scene.add(ground);
 scene.add(cube);
+scene.add(cube2);
+scene.add(ball);
 
 // lights
 
-const dirLight1 = new THREE.DirectionalLight(0xffffff);
-dirLight1.position.set(1, 1, 1);
+const dirLight1 = new THREE.PointLight(0xffffff);
+dirLight1.position.set(5, 5, 5);
 dirLight1.castShadow = true;
 scene.add(dirLight1);
 
-const ambientLight = new THREE.AmbientLight(0x222222);
+const lightBall = new THREE.Mesh(new THREE.SphereGeometry(0.2), new THREE.MeshToonMaterial({color: 0xeeee00}));
+dirLight1.add(lightBall);
+
+const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
 function animate(frame: number) {
   requestAnimationFrame(animate);
 
-  // cube.rotation.x = frame / 1000;
-  // cube.rotation.y = frame / 1000;
+  dirLight1.position.x = Math.sin(frame / 1000) * 5;
+  dirLight1.position.z = Math.cos(frame / 1000) * 5;
 
   renderer.render(scene, camera);
 }
+
+window.onresize = () => {
+  const newAspect = window.innerWidth / window.innerHeight;
+  camera.left = frustumSize * newAspect / -2;
+  camera.right = frustumSize * newAspect / 2;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
 
 animate(window.performance.now());
 </script>
